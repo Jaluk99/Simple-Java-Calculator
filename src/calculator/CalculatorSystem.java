180349d21f81e5ad1bd44079456ddb197ab1fb38
Johnny Lukas <johnnylukas99@gmail.com>
2017-10-28 13:53:12 +0200

    Update v.1.1

--- src/calculator/CalculatorSystem.java
+++ src/calculator/CalculatorSystem.java
@@ -10,12 +10,6 @@ public class CalculatorSystem {
 
     public CalculatorSystem() {}
 
-    public CalculatorSystem(int a, int b, String typ) {
-        this.a = a;
-        this.b = b;
-        this.typ = typ;
-    }
-
     public void msg() {
         System.out.println("Wynik " + typ + " to " + wynik);
     }
@@ -23,10 +17,10 @@ public class CalculatorSystem {
     public void msgDelta(int i) {
         switch (i) {
             case 1:
-                System.out.println("Wynik " + typ + " to x0: " + getX0());
+                System.out.println("Wynik " + typ + " to x0: " + x0);
                 break;
             case 2:
-                System.out.println("Wynik " + typ + " to x1: " + getX1() + " | x2: " + getX2());
+                System.out.println("Wynik " + typ + " to x1: " + x1 + " | x2: " + x2);
                 break;
             case 3:
                 System.out.println("Delta nie ma rozwiazania!");
@@ -38,14 +32,14 @@ public class CalculatorSystem {
 
         Math.pow(b1, 2);
         delta = b1 - 4 * a1 * c1;
-        setWynik((int) Math.sqrt(delta));
+        wynik = ((int) Math.sqrt(delta));
 
         if(wynik == 0) {
             setX0((-b1 / (2 * a1)));
             msgDelta(1);
         } else if(wynik > 0) {
-            setX1((-b1 + getWynik()) / (2 * a1));
-            setX2((-b1 - getWynik()) / (2 * a1));
+            setX1((-b1 + wynik) / (2 * a1));
+            setX2((-b1 - wynik) / (2 * a1));
             msgDelta(2);
         } else if(wynik < 0) {
             msgDelta(3);
@@ -55,25 +49,25 @@ public class CalculatorSystem {
     }
 
     public void type(){
-        switch(getTyp()){
+        switch(typ){
             case "mnozenie":
                 setTyp("Mnozenia");
-                setWynik(getA()*getB());
+                setWynik(a * b);
                 msg();
                 break;
             case "dzielenie":
                 setTyp("Dzielenia");
-                setWynik(getA()/getB());
+                setWynik(a / b);
                 msg();
                 break;
             case "dodawanie":
                 setTyp("Dodawania");
-                setWynik(getA()+getB());
+                setWynik(a + b);
                 msg();
                 break;
             case "odejmowanie":
                 setTyp("Odejmowania");
-                setWynik(getA()-getB());
+                setWynik(a - b);
                 msg();
                 break;
             case "delta":
@@ -85,80 +79,40 @@ public class CalculatorSystem {
         }
     }
 
-    public int getA() {
-        return a;
-    }
-
     public void setA(int a) {
         this.a = a;
-    }
-
-    public int getB() {
-        return b;
     }
 
     public void setB(int b) {
         this.b = b;
     }
 
-    public String getTyp() {
-        return typ;
-    }
-
     public void setTyp(String typ) {
         this.typ = typ;
-    }
-
-    public int getWynik() {
-        return wynik;
     }
 
     public void setWynik(int wynik) {
         this.wynik = wynik;
     }
 
-    public double getX1() {
-        return x1;
-    }
-
     public void setX1(double x1) {
         this.x1 = x1;
-    }
-
-    public double getX2() {
-        return x2;
     }
 
     public void setX2(double x2) {
         this.x2 = x2;
     }
 
-    public double getX0() {
-        return x0;
-    }
-
     public void setX0(double x0) {
         this.x0 = x0;
-    }
-
-    public double getA1() {
-        return a1;
     }
 
     public void setA1(double a1) {
         this.a1 = a1;
     }
 
-    public double getB1() {
-        return b1;
-    }
-
     public void setB1(double b1) {
         this.b1 = b1;
-    }
-
-    public double getC1() {
-        return c1;
     }
 
     public void setC1(double c1) {
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -3,16 +3,19 @@ package calculator;
 public class Main {
     public static void main(String[] args){
         CalculatorSystem system = new CalculatorSystem();
+        //Parametry dla mnozenia, odejmowania, dodawania, dzielenia
         system.setA(-10);
         system.setB(8);
 
+        //Parametry dla delty
         system.setA1(-5);
         system.setB1(27);
         system.setC1(4);
 
+        //Ustaw typ (mnozenie, dodawania, odejmowanie, dzielnie, delta)
         system.setTyp("delta");
 
+        //Metoda licząca
         system.type();
-
     }
 }
